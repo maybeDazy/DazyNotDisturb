6065c3c006d5f9fdd64f9b9ace6324d50c5e8c45
kim minjun <24055@baekseok.hs.kr>
2026-08-31 10:28:52 +0900

    Fix: always build dynamic specifiers from LSApplicationWorkspace

--- DazyNotDisturb/DazyNotDisturbPrefs/DNBAppListController.m
+++ DazyNotDisturb/DazyNotDisturbPrefs/DNBAppListController.m
@@ -6,99 +6,22 @@
 
 @implementation DNBAppListController {
     NSMutableArray<NSString *> *_allBundleIDs;
-    NSArray<NSString *> *_selectedBundleIDs;
-    NSArray *_specifiers;
     BOOL _loaded;
 }
 
+- (instancetype)init {
+    self = [super init];
+    if (self) {
+        _allBundleIDs = [NSMutableArray array];
+    }
+    return self;
+}
+
 - (NSArray *)specifiers {
-    if (!_specifiers) {
-        _specifiers = [self loadSpecifiersFromPlistName:@"TargetApps" target:self];
+    if (!_loaded) {
+        [self loadInstalledApps];
+        _loaded = YES;
     }
-    return _specifiers;
-}
-
-- (id)readPreferenceValue:(PSSpecifier *)specifier {
-    NSString *key = [specifier propertyForKey:@"key"];
-    NSUserDefaults *d = [[NSUserDefaults alloc] initWithSuiteName:@"Disturb.Dazy.Pro"];
-    return [d objectForKey:key] ?: @NO;
-}
-
-- (void)setPreferenceValue:(id)value specifier:(PSSpecifier *)specifier {
-    NSString *key = [specifier propertyForKey:@"key"];
-    NSUserDefaults *d = [[NSUserDefaults alloc] initWithSuiteName:@"Disturb.Dazy.Pro"];
-    [d setObject:value forKey:key];
-    [d synchronize];
-
-    if (key && [key hasPrefix:@"App_"]) {
-        NSString *bundleID = [key substringFromIndex:4];
-        [self updateSelectedBundleIDs];
-        NSMutableArray *arr = [d arrayForKey:@"DND_SelectedApps"] ? [[d arrayForKey:@"DND_SelectedApps"] mutableCopy] : [NSMutableArray array];
-        if ([value boolValue]) {
-            if (![arr containsObject:bundleID]) [arr addObject:bundleID];
-        } else {
-            [arr removeObject:bundleID];
-        }
-        [d setObject:arr forKey:@"DND_SelectedApps"];
-        [d synchronize];
-    }
-}
-
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    _allBundleIDs = [NSMutableArray array];
-    [self loadInstalledApps];
-    [self updateSelectedBundleIDs];
-}
-
-- (void)viewWillAppear:(BOOL)animated {
-    [super viewWillAppear:animated];
-    if (_loaded) {
-        [self updateSelectedBundleIDs];
-        [self rebuildAppListSpecifiers];
-    }
-}
-
-- (void)loadInstalledApps {
-    Class wsClass = NSClassFromString(@"LSApplicationWorkspace");
-    if (!wsClass) return;
-    @try {
-        id ws = [wsClass performSelector:@selector(defaultWorkspace)];
-        NSArray *apps = [ws performSelector:@selector(allInstalledApplications)];
-        for (id proxy in apps) {
-            NSString *bid = nil;
-            NSString *name = nil;
-            if ([proxy respondsToSelector:@selector(bundleIdentifier)]) {
-                bid = [proxy performSelector:@selector(bundleIdentifier)];
-            }
-            if ([proxy respondsToSelector:@selector(localizedName)]) {
-                name = [proxy performSelector:@selector(localizedName)];
-            }
-            if (bid.length > 0 && name.length > 0) {
-                [_allBundleIDs addObject:bid];
-            }
-        }
-        [_allBundleIDs sortUsingComparator:^NSComparisonResult(NSString *a, NSString *b) {
-            return [a compare:b];
-        }];
-    } @catch (NSException *e) {
-        NSLog(@"[DazyNotDisturb] loadInstalledApps exception: %@", e);
-    }
-}
-
-- (void)updateSelectedBundleIDs {
-    NSUserDefaults *d = [[NSUserDefaults alloc] initWithSuiteName:@"Disturb.Dazy.Pro"];
-    NSArray *arr = [d arrayForKey:@"DND_SelectedApps"];
-    _selectedBundleIDs = arr ?: @[];
-}
-
-- (void)rebuildAppListSpecifiers {
-    [self setValue:nil forKey:@"_specifiers"];
-    _specifiers = nil;
-    _specifiers = [self buildDynamicSpecifiers];
-}
-
-- (NSArray *)buildDynamicSpecifiers {
     NSMutableArray *specs = [NSMutableArray array];
     NSUserDefaults *d = [[NSUserDefaults alloc] initWithSuiteName:@"Disturb.Dazy.Pro"];
 
@@ -109,26 +32,84 @@
                                                                 detail:nil
                                                                  cell:PSGroupCell
                                                                  edit:nil];
-    [headerGroup setProperty:@"설치된 앱 목록입니다. 끄면 방해금지 모드와 무관하게 알림이 정상 표시됩니다." forKey:@"footerText"];
+    [headerGroup setProperty:[NSString stringWithFormat:@"설치된 앱 %lu개. 끌 앱은 알림이 정상 표시됩니다.", (unsigned long)_allBundleIDs.count]
+                      forKey:@"footerText"];
     [specs addObject:headerGroup];
 
+    NSUInteger idx = 0;
     for (NSString *bid in _allBundleIDs) {
-        PSSpecifier *s = [PSSpecifier preferenceSpecifierNamed:bid
-                                                          target:self
-                                                             set:@selector(setPreferenceValue:specifier:)
-                                                             get:@selector(readPreferenceValue:)
-                                                          detail:Nil
-                                                           cell:PSSwitchCell
-                                                           edit:Nil];
-        [s setProperty:bid forKey:@"label"];
-        [s setProperty:[@"App_" stringByAppendingString:bid] forKey:@"key"];
-        [s setProperty:@"Disturb.Dazy.Pro" forKey:@"defaults"];
-        BOOL on = [d boolForKey:[@"App_" stringByAppendingString:bid]];
-        [s setProperty:@(on) forKey:@"default"];
-        [specs addObject:s];
+        @autoreleasepool {
+            PSSpecifier *s = [PSSpecifier preferenceSpecifierNamed:bid
+                                                              target:self
+                                                                 set:@selector(setPreferenceValue:specifier:)
+                                                                 get:@selector(readPreferenceValue:)
+                                                              detail:nil
+                                                               cell:PSSwitchCell
+                                                               edit:nil];
+            [s setProperty:bid forKey:@"label"];
+            [s setProperty:[@"App_" stringByAppendingString:bid] forKey:@"key"];
+            [s setProperty:@"Disturb.Dazy.Pro" forKey:@"defaults"];
+            [s setProperty:@YES forKey:@"default"];
+            BOOL on = [d boolForKey:[@"App_" stringByAppendingString:bid]];
+            (void)on;
+            [specs addObject:s];
+        }
+        idx++;
     }
-    _loaded = YES;
     return specs;
+}
+
+- (id)readPreferenceValue:(PSSpecifier *)specifier {
+    NSString *key = [specifier propertyForKey:@"key"];
+    NSUserDefaults *d = [[NSUserDefaults alloc] initWithSuiteName:@"Disturb.Dazy.Pro"];
+    return @([d boolForKey:key]);
+}
+
+- (void)setPreferenceValue:(id)value specifier:(PSSpecifier *)specifier {
+    NSString *key = [specifier propertyForKey:@"key"];
+    NSUserDefaults *d = [[NSUserDefaults alloc] initWithSuiteName:@"Disturb.Dazy.Pro"];
+    [d setObject:value forKey:key];
+    [d synchronize];
+
+    NSArray *arr = [d arrayForKey:@"DND_SelectedApps"] ?: @[];
+    NSMutableArray *newArr = [arr mutableCopy];
+    if (key && [key hasPrefix:@"App_"]) {
+        NSString *bundleID = [key substringFromIndex:4];
+        if ([value boolValue]) {
+            if (![newArr containsObject:bundleID]) [newArr addObject:bundleID];
+        } else {
+            [newArr removeObject:bundleID];
+        }
+        [d setObject:newArr forKey:@"DND_SelectedApps"];
+        [d synchronize];
+    }
+}
+
+- (void)loadInstalledApps {
+    Class wsClass = NSClassFromString(@"LSApplicationWorkspace");
+    if (!wsClass) {
+        NSLog(@"[DazyNotDisturb] LSApplicationWorkspace class not found");
+        return;
+    }
+    @try {
+        id ws = [wsClass performSelector:@selector(defaultWorkspace)];
+        NSArray *apps = [ws performSelector:@selector(allInstalledApplications)];
+        for (id proxy in apps) {
+            NSString *bid = nil;
+            if ([proxy respondsToSelector:@selector(bundleIdentifier)]) {
+                bid = [proxy performSelector:@selector(bundleIdentifier)];
+            }
+            if (bid.length > 0) {
+                [_allBundleIDs addObject:bid];
+            }
+        }
+        [_allBundleIDs sortUsingComparator:^NSComparisonResult(NSString *a, NSString *b) {
+            return [a compare:b];
+        }];
+        NSLog(@"[DazyNotDisturb] loaded %lu apps", (unsigned long)_allBundleIDs.count);
+    } @catch (NSException *e) {
+        NSLog(@"[DazyNotDisturb] loadInstalledApps exception: %@", e);
+    }
 }
 
 - (NSString *)navigationTitle {
